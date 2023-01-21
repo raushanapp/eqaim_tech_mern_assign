1d72ee431417dfe2a7d04d53cbb9dbd0344a95c8
raushan kuamr <sraushan890@gmail.com>
2023-01-22 04:16:08 +0530

    singleBlogs done

--- client/src/Components/BlogCard.jsx
+++ client/src/Components/BlogCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import style from "./blogsCard.module.css";
 
 export const BlogCard = ({ title,id }) => {
-    console.log(title,"title")
+    // console.log(title,"title")
     return (
         <Link to={`/singleblog/${id}`}>
             <div className={style.blogCard}>
--- client/src/Pages/SingleBlog.jsx
+++ client/src/Pages/SingleBlog.jsx
@@ -1,12 +1,43 @@
-import { useParams } from "react-router-dom"
-
-
+import { Link, useParams } from "react-router-dom"
+import style from "./singleBlog.module.css";
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react";
+import {RiHome2Line} from "react-icons/ri"
 export const SingleBlog = () => {
-    const {id} = useParams();
-    console.log(id);
+    const { id } = useParams();
+    const [blog, setBlog] = useState({});
+    const getSingleBlog = () => {
+        axios.get(`http://localhost:2300/blogs/${id}`)
+            .then((res) => setBlog(res.data.blog))
+            .catch((error) => setBlog(error))
+    };
+
+    useEffect(() => {
+        getSingleBlog();
+    },[])
+    console.log(blog);
     return (
-        <div>
-            single
+        <div className={style.singleBlogMain}>
+            <div className={style.headerDiv}>
+                <h1 className={style.header}>Eqaim Blog</h1>
+            </div>
+            <div className={style.card}>
+                <div className={style.btnDiv}>
+                    <Link to='/'>
+                        <button className={style.btn}>
+                            <RiHome2Line style={{
+                                fontSize:"45px"
+                            }}/>
+                        </button>
+                    </Link>
+                </div>
+                
+                <div className={style.blogDiv}>
+                    <h1 className={style.blogHeader}>{blog.title}</h1>
+                    <p className={style.blogPost}>{blog.post}</p>
+                </div>
+            </div>
         </div>
     )
 }
